65a89130473bd3a873c812d670c7a5063e98b844
Kotelnikovekb <49716608+Kotelnikovekb@users.noreply.github.com>
2023-04-09 11:15:28 +0500

    add read me

--- lib/ru_phone_formatter.dart
+++ lib/ru_phone_formatter.dart
@@ -10,7 +10,7 @@ class RuPhoneInputFormatter extends TextInputFormatter{
   bool _isRu=false;
 
 
-  SmartPhoneInputFormatter({
+  RuPhoneInputFormatter({
     String? initialText,
   }) {
     if (initialText != null) {
